e4d22bbaa8272a13d198bfed89d919bd0a3740cc
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-09-15 17:03:59 +0300

    Test if it works

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to AudioCloud! 12:22</h2>
+          <h2>Welcome to AudioCloud! 17:03</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

ad43b53e5ec879f81272cec3c1b4aa76adfb46e6
Zilvinas Abromavicius <Zilvinas.Abromavicius@devbridge.com>
2017-09-28 13:35:16 +0300

    Added TodoMVC as an example template.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+// import 'semantic-ui-css/semantic.min.css';
 
-import './index.css';
-import App from './App';
+// import './index.css';
+
+import App from './containers/App';
+import reducer from './reducers';
+import 'todomvc-app-css/index.css';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();

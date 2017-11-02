f157d31c03384252f86649a9a3026f6e6d361ad0
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-11-02 19:59:55 +0200

    fasdf

--- src/auth/Authenticate.js
+++ src/auth/Authenticate.js
@@ -8,9 +8,11 @@ class Authenticate extends React.Component {
   };
 
   async componentDidMount() {
+    console.log('aa');
     debugger;
     const isAuthenticated = await auth.handleAuthentication();
-
+    debugger;
+    console.log('isAuthenticated:', isAuthenticated);
     this.setState({isAuthenticated});
   }
 

a4bc661084c5cc5dfd7adcf811be4a3c4c4ff4e1
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-11-02 20:07:25 +0200

    Fix client side routing.

--- src/auth/Authenticate.js
+++ src/auth/Authenticate.js
@@ -8,11 +8,7 @@ class Authenticate extends React.Component {
   };
 
   async componentDidMount() {
-    console.log('aa');
-    debugger;
     const isAuthenticated = await auth.handleAuthentication();
-    debugger;
-    console.log('isAuthenticated:', isAuthenticated);
     this.setState({isAuthenticated});
   }
 

d7854abc9512a1b8135711ee4e6103f36db2615b
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-11-05 15:03:48 +0200

    Fix url bug.

--- src/selectors/RouterSelectors.js
+++ src/selectors/RouterSelectors.js
@@ -4,12 +4,12 @@ const getPathname = state => state.router.location.pathname;
 
 export const getSongId = createSelector(
   getPathname,
-  pathname => parseInt(pathname.split('/')[2], 10)
+  pathname => parseInt(pathname.split('/')[3], 10)
 );
 
 export const getPlaylistSongId = createSelector(
   getPathname,
-  pathname => parseInt(pathname.split('/')[3], 10)
+  pathname => parseInt(pathname.split('/')[4], 10)
 );
 
 export const getPlaylistId = getSongId;

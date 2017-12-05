d888ecfd3873dcb58803b7be6e63fa2befba2832
Zilvinas Abromavicius <abromavicius.zilvinas@gmail.com>
2017-12-05 14:10:08 +0200

    Should render by chunks. Will test when Like is implemented.

--- src/library/ShowAllList.js
+++ src/library/ShowAllList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import BlackLink from '../common/BlackLink';
 import {Header, Image, Loader, Segment} from 'semantic-ui-react';
+import {chunk as _chunk} from 'lodash';
 
 import MainButton from '../common/MainButton';
 
@@ -16,6 +17,25 @@ class ShowAllList extends Component {
     this.setState({isShowingAll: true})
   };
 
+  renderChunk = (elements, i) => (
+    <Segment.Group horizontal key={i}>
+      {
+        elements.map((element, j) => (
+          <Segment key={j}>
+            <Image size='tiny' src={element.pictureUrl || 'http://via.placeholder.com/1024x1024'}/>
+            <Header as='h5'>
+              <BlackLink
+                to={element.linkUrl}
+              >
+                {element.title}
+              </BlackLink>
+            </Header>
+          </Segment>
+        ))
+      }
+    </Segment.Group>
+  );
+
   render() {
     const {isShowingAll} = this.state;
     const {title, elements} = this.props;
@@ -29,24 +49,14 @@ class ShowAllList extends Component {
       );
     }
 
+    const chunkSize = 4;
+    const chunkedElements = _chunk(elements, chunkSize);
+
     return (
       <Segment>
         <Header as='h2'>{title}</Header>
-        <Segment.Group horizontal>
-          {
-            elements.map((element, i) => (
-              <Segment key={i}>
-                <Image size='tiny' src={element.pictureUrl || 'http://via.placeholder.com/1024x1024'}/>
-                <Header as='h5'>
-                  <BlackLink
-                    to={element.linkUrl}
-                  >
-                    {element.title}
-                  </BlackLink>
-                </Header>
-              </Segment>
-            ))
-          }
+        <Segment.Group>
+          {chunkedElements.map(this.renderChunk)}
         </Segment.Group>
         <div>
           {isShowingAll || <MainButton onClick={this.handleShowAll}>Show All</MainButton>}
